d396e3467d2aa95df47f8eb97e3d7a3abd28767c
perjermer <pierce.morris1998@gmail.com>
2019-01-10 15:38:33 +0000

    Updates

---
+++ precache-manifest.092431e549f997034c9ffc1a7b32bfc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "9161e81aad4e6d84942d",
+    "url": "/portfolio/static/js/main.9161e81a.chunk.js"
+  },
+  {
+    "revision": "62c963d5b7656b9bc3f0",
+    "url": "/portfolio/static/js/1.62c963d5.chunk.js"
+  },
+  {
+    "revision": "9161e81aad4e6d84942d",
+    "url": "/portfolio/static/css/main.4ab4fa39.chunk.css"
+  },
+  {
+    "revision": "b4bde97231596c7a9cb64d4f0449f6c4",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eecf317c2f50bf7f515ccddfc803f763.js"
+  "/portfolio/precache-manifest.092431e549f997034c9ffc1a7b32bfc7.js"
 );
 
 workbox.clientsClaim();

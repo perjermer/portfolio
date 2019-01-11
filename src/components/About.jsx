873f75085801f2974ca9d4b383901cb3b23558ac
perjermer <pierce.morris1998@gmail.com>
2019-01-11 10:33:38 +0000

    add social media links and pop outs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,18 @@ const About = () => {
       <Title title="About" desc="All you need to know about me." />
       <img id="me" src={require('../public/me2.jpg')} alt="me" />
       <div className="text">
-          Started programming back in 2015; learnt the fundamentals using Python, creating very simple algorithms and desktop shortcuts. Found an interest into the many aspects of web development from backend to frontend and everything in between.
+        Started programming back in 2015; learnt the fundamentals using Python, creating very simple
+        algorithms and desktop shortcuts. Found an interest into the many aspects of web development
+        from backend to frontend and everything in between.
+        <br />
+        <br />
+        I had a taste of full stack development first with PHP as part of a first year University
+        project, which inspired me to look into other web development frameworks/libraries i.e. Node & React.
+        Since then, I've been honing my skills whilst working on my degree.
+        <br />
+        <br />
+        I am currently studying Computer Science & Artificial Intelligence & learning Japanese on the side
+        with the <a href="http://www.alljapaneseallthetime.com/blog/all-japanese-all-the-time-ajatt-how-to-learn-japanese-on-your-own-having-fun-and-to-fluency/">AJATT</a> method.
       </div>
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from "./reusable/Title";
+import { SocialIcon } from 'react-social-icons';
 
 const Contact = () => {
   return (
     <div>
-      <div>
-        <Title title="Contact" desc="Get in touch." />
+      <Title title="Contact" desc="Get in touch." />
+      <div className="body-content">
+        You can find me on several social media
+        <br />
+        <br />
+        <SocialIcon className="space grow" url="https://twitter.com/perjermer" />
+        <SocialIcon className="space grow" url="https://www.linkedin.com/in/pierce-morris-166698136/" />
+        <SocialIcon className="space grow" url="https://github.com/perjermer" />
       </div>
     </div>
   );

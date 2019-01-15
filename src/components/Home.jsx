d682fa7eda127cd1d2ed0483cf76264e77f568e3
perjermer <pierce.morris1998@gmail.com>
2019-01-15 14:05:16 +0000

    simplified website & removed unnecessary pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Title from "./reusable/Title";
+import { SocialIcon } from 'react-social-icons';
 
-const About = () => {
+const Home = () => {
   return (
     <div>
-      <Title title="About" desc="All you need to know about me." />
+      <Title title="Pierce James Morris" desc="Aspiring web developer, avid Japanese learner, Comp Sci university student" />
       <img id="me" src={require('../public/me2.jpg')} alt="me" />
       <div className="text">
         Started programming back in 2015; learnt the fundamentals using Python, creating very simple
@@ -23,8 +24,16 @@ const About = () => {
         I am currently studying Computer Science & Artificial Intelligence & learning Japanese on the side
         with the <a href="http://www.alljapaneseallthetime.com/blog/all-japanese-all-the-time-ajatt-how-to-learn-japanese-on-your-own-having-fun-and-to-fluency/">AJATT</a> method.
       </div>
+      <br />
+      <br />
+      You can find me on several social media
+      <br />
+      <br />
+      <SocialIcon className="space grow" url="https://twitter.com/perjermer" />
+      <SocialIcon className="space grow" url="https://www.linkedin.com/in/pierce-morris-166698136/" />
+      <SocialIcon className="space grow" url="https://github.com/perjermer" />
     </div>
   );
 }
 
-export default About;
+export default Home;
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink, Link } from "react-router-dom";
 import Portfolio from "./Portfolio";
-import About from "./About";
-import Contact from "./Contact";
+import Home from "./Home";
 
 const NavMenu = () => {
   return (
@@ -12,19 +11,12 @@ const NavMenu = () => {
           <ul className="nav">
             <li><Link to="/" className="name">Pierce James Morris</Link></li>
             <li>
-              <NavLink to="/contact/" className="nav-item hover" activeClassName="active">Contact</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about/" className="nav-item hover" activeClassName="active">About</NavLink>
-            </li>
-            <li>
               <NavLink to="/portfolio/" className="nav-item hover" activeClassName="active">Portfolio</NavLink>
             </li>
           </ul>
         </div>
 
-        <Route path="/about/" component={About} />
-        <Route path="/contact/" component={Contact} />
+        <Route exact path="/" component={Home} />
         <Route path="/portfolio/" exact component={Portfolio} />
       </div>
     </Router>
